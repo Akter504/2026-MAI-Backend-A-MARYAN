d8393d2d47df791d7bcf959f462ae058569a936e
Maryan <maryan.sasha@bk.ru>
2026-03-18 12:13:33 +0300

    Implement LRU cache and main test

---
+++ code/main.py
@@ -0,0 +1,15 @@
+from cache import LRUCache
+
+def main():
+    cache = LRUCache(100)
+    cache.set('Jesse', 'Pinkman')
+    cache.set('Walter', 'White')
+    cache.set('Jesse', 'James')
+    
+    print(cache.get('Jesse'))
+    
+    cache.rem('Walter')
+    print(f"Walter after rem: '{cache.get('Walter')}'")
+
+if __name__ == "__main__":
+    main()
